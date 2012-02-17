f56a46d42ac3eab6fade6cb19eb8853b9d10d4f2
Thomas Metschke <thomas.metschke@gmail.com>
2012-02-17 18:57:10 +0100

    remove spaces

--- vsftpd/recipes/setup.rb
+++ vsftpd/recipes/setup.rb
@@ -5,12 +5,12 @@ template "/etc/vsftpd.conf" do
   mode "0660"
 end
 
-user "#{node [:vsftpd2][:user]}" do
+user "#{node[:vsftpd2][:user]}" do
   comment "FTP user"
   gid "users"
   home "/home/nuestratv"
   shell "/bin/zsh"
-  password node [:vsftpd2][:pwd_hash]
+  password node[:vsftpd2][:pwd_hash]
 end
 
 execute "restart vsftpd" do
