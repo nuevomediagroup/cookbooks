78534eee0604b7564fb024f4a5db0afe1fbbb4f6
Thomas Metschke <thomas.metschke@gmail.com>
2012-02-29 10:21:46 +0100

    add nfs recipes

---
+++ nfs/attributes/default.rb
@@ -0,0 +1,5 @@
+default[:nfs][:exports] = ["/vol/data"]
+default[:nfs][:clients] = scalarium[:roles]['php-app'][:instances].values.map{|config| config[:private_ip]} rescue []
+default[:nfs][:server] = scalarium[:roles]['wp-master'][:instances].values.map{|config| config[:private_ip]}.first rescue nil
+
+default[:nfs][:mount_options] = %w(rsize=32768,wsize=32768,bg,hard,nfsvers=3,intr,tcp)
