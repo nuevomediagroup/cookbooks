ed2eabeb55f9356f3843245148da5337081fc733
Thomas Metschke <thomas.metschke@gmail.com>
2012-02-29 11:25:11 +0100

    update nfs recipes

--- nfs/recipes/client.rb
+++ nfs/recipes/client.rb
@@ -1,20 +1,25 @@
 unless node[:scalarium][:instance][:roles].include?('nfs')
-  package "nfs-common"
+  
+  if node[:nfs][:server]
+    package "nfs-common"
 
-  node[:nfs][:exports].each do |mount_point|
-    directory mount_point do
-      recursive true
-      mode "0777"
+    node[:nfs][:exports].each do |mount_point|
+      directory mount_point do
+        recursive true
+        mode "0777"
+      end
+
+      mount mount_point do
+        fstype "nfs"
+        options node[:nfs][:mount_options]
+        device "#{node[:nfs][:server]}:#{mount_point}"
+        dump 0
+        pass 0
+        # mount and add to fstab. set to 'disable' to remove it
+        action [:enable, :mount]
+      end
     end
 
-    mount mount_point do
-      fstype "nfs"
-      options node[:nfs][:mount_options]
-      device "#{node[:nfs][:server]}:#{mount_point}"
-      dump 0
-      pass 0
-      # mount and add to fstab. set to 'disable' to remove it
-      action [:enable, :mount]
-    end
   end
+
 end
